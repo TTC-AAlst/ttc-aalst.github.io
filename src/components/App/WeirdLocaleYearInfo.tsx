ba1dce435c737d447721ab8b9c59d78f37855139
Wouter Van Schandevijl <woutervs@hotmail.com>
2025-01-05 14:11:56 +0100

    changed intro

--- src/components/App/WeirdLocaleYearInfo.tsx
+++ src/components/App/WeirdLocaleYearInfo.tsx
@@ -3,14 +3,12 @@ import { t } from '../../locales';
 
 export const WeirdLocaleYearInfo = ({params}: any) => ( // eslint-disable-line
   <div>
-    <h2>
-      WIJZIGING NAAM
-    </h2>
+    <h3>WIJZIGING NAAM</h3>
     <b>Opgelet: TTC Erembodegem heet vanaf heden TTC Aalst</b>
 
     <br />
 
-    <h3>Sportzaal Technigo</h3>
+    <h3>KTA Technigo Sporthal - De Voorstad</h3>
     <b>{params.location}</b>
     <br />
     <br />{t('clubs.training.trainingDays2')}
