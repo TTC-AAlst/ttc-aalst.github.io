fce5b6eac4ca031bdbbb3298d8839d33a119e80b
Wouter Van Schandevijl <wouter.vanschandevijl@fluxys.com>
2025-04-24 13:55:12 +0200

    more achievements that may be implemented later

--- src/components/other/EndOfSeason/PlayerAchievements.ts
+++ src/components/other/EndOfSeason/PlayerAchievements.ts
@@ -9,6 +9,31 @@ import { getTeamUndefeatedStreak } from './achievements/getTeamUndefeatedStreak'
 import { getUndefeatedStreak } from './achievements/getUndefeatedStreak';
 import * as achievement from './achievements/otherAchievements';
 
+// TODO: List of possible achievements that can still be added
+
+// Players
+// 🚗 De Reiziger: "Meeste uitwedstrijden gespeeld"
+
+// Dubbels
+// Beste dubbel partners: meest dubbel wedstrijden gewonnen
+// Doubles Clutcher: Beste dubbel partner: speler met meest gewonnen dubbels
+
+// Belles
+// These are only possible if we start to also keep track of the sets, which is only possible for Vttl
+// 🤬 Net Niet: meeste verloren belles met het kleinste verschil
+// 👻 Tiebreak Terror: "Meeste belles gespeeld die eindigden in 11–9 of 12–10"
+// 🧷 Speld op de draad – spannend en precies: meeste gewonnen belles met het kleinste verschil
+// 🔙 Comeback King: "Meeste belles gewonnen na 0–2 achterstand"
+
+// Team
+// 🧱 "Onverslaanbaar Thuis": Team met de meeste thuisoverwinningen
+// ✈️ "Uitploeg van het Jaar": Meeste overwinningen op verplaatsing
+// 😤 "Comeback Kings": Winst behaald na minstens 3–0 achterstand(games)
+// 🎯 "Clutch Factor": Team dat de meeste beslissende matchen won(laatste game beslist match)
+// 🔁 "Revanche!": Team dat een tegenstander versloeg na eerder dat seizoen te hebben verloren
+// 🧙 "Consistency is Key": Team dat het vaakst won met dezelfde score(bv. 12–4 of 7–3)
+
+
 const allAchievements: {[key: string]: ((playerStats: ITeamPlayerStats[], matches: IMatch[]) => achievement.AchievementInfo)[]} = {
   Vttl: [
     achievement.getHighestJumper.bind(this, 'Vttl'),
