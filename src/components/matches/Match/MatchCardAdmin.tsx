54b56498ea82560fce9a8306a29ec90e01176486
Wouter Van Schandevijl <woutervs@hotmail.com>
2025-08-30 14:55:37 +0200

    matchCard: admin: console.log the opstellingen

--- src/components/matches/Match/MatchCardAdmin.tsx
+++ src/components/matches/Match/MatchCardAdmin.tsx
@@ -9,6 +9,13 @@ type MatchCardAdminProps = {
 
 export const MatchCardAdmin = ({match}: MatchCardAdminProps) => {
   const dispatch = useTtcDispatch();
+
+  const playerDetails = match.players
+    .slice()
+    .sort((a, b) => `${a.status}-${a.alias}`.localeCompare(`${b.status}-${b.alias}`))
+    .map(ply => `${ply.status}: ${ply.alias}`);
+  console.log('playerDetails', playerDetails);
+
   return (
     <div style={{padding: 7}}>
       <button type="button" onClick={() => dispatch(frenoyMatchSync({match, forceSync: true}))} className="btn btn-outline-secondary pull-right">
@@ -16,6 +23,7 @@ export const MatchCardAdmin = ({match}: MatchCardAdminProps) => {
       </button>
 
       ID={match.id}<br />FrenoyId={match.frenoyMatchId}
+      <br />Block={match.block}
 
       <div style={{clear: 'both'}} />
 
