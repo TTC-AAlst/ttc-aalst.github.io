213921ab06fb3131001110ca0e1b242fc1bf583f
Wouter Van Schandevijl <woutervs@hotmail.com>
2025-09-13 09:55:06 +0200

    Team players: fix avatar layout when there are many players in a team

--- src/components/teams/controls/TeamPlayerAvatars.tsx
+++ src/components/teams/controls/TeamPlayerAvatars.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PlayerAvatar from '../../players/PlayerAvatar';
 import { IPlayer, ITeam } from '../../../models/model-interfaces';
 import { t } from '../../../locales';
@@ -10,44 +10,48 @@ type TeamPlayerAvatarsProps = {
 
 const getSortKey = (player: IPlayer, team: ITeam) => player.getCompetition(team.competition).position;
 
-export class TeamPlayerAvatars extends Component<TeamPlayerAvatarsProps> {
-  render() {
-    const {team} = this.props;
-    const teamPlayers = team.getPlayers('standard')
-      .sort((a, b) => getSortKey(a.player, team) - getSortKey(b.player, team));
+const containerStyle: React.CSSProperties = {
+  justifyContent: 'center',
+  display: 'flex',
+  marginBottom: 16,
+  flexWrap: 'wrap',
+};
 
-    return (
-      <div style={{justifyContent: 'center', display: 'flex', marginBottom: 16, ...this.props.style}}>
-        {teamPlayers.map(ply => {
-          let tooltip = ply.player.name;
+export const TeamPlayerAvatars = ({team, style}: TeamPlayerAvatarsProps) => {
+  const teamPlayers = team.getPlayers('standard')
+    .sort((a, b) => getSortKey(a.player, team) - getSortKey(b.player, team));
 
-          const isCaptain = team.isCaptain(ply.player);
-          if (isCaptain) {
-            tooltip = `${t('player.teamCaptain')}: ${tooltip}`;
-          }
+  return (
+    <div style={{...containerStyle, ...style}}>
+      {teamPlayers.map(ply => {
+        let tooltip = ply.player.name;
 
-          const comp = ply.player.getCompetition(team.competition);
-          if (comp.ranking) {
-            tooltip += ` (${comp.ranking})`;
-          }
+        const isCaptain = team.isCaptain(ply.player);
+        if (isCaptain) {
+          tooltip = `${t('player.teamCaptain')}: ${tooltip}`;
+        }
 
-          const avatarStyle = {
-            marginRight: 16,
-            boxShadow: `3px 3px 3px ${isCaptain ? '#CD7F32' : '#888888'}`,
-            flex: 1,
-          };
+        const comp = ply.player.getCompetition(team.competition);
+        if (comp.ranking) {
+          tooltip += ` (${comp.ranking})`;
+        }
 
-          return (
-            <PlayerAvatar
-              key={ply.player.id}
-              player={ply.player}
-              style={avatarStyle}
-              tooltip={tooltip}
-              tooltipPlacement="bottom"
-            />
-          );
-        })}
-      </div>
-    );
-  }
-}
+        const avatarStyle = {
+          marginRight: 16,
+          boxShadow: `3px 3px 3px ${isCaptain ? '#CD7F32' : '#888888'}`,
+          flex: 1,
+        };
+
+        return (
+          <PlayerAvatar
+            key={ply.player.id}
+            player={ply.player}
+            style={avatarStyle}
+            tooltip={tooltip}
+            tooltipPlacement="bottom"
+          />
+        );
+      })}
+    </div>
+  );
+};
