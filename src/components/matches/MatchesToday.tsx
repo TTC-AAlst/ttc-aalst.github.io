e8a2898c3d2e754900d25b078ff6004edf5b1c33
Wouter Van Schandevijl <woutervs@hotmail.com>
2025-12-06 16:50:47 +0100

    use same component for bigger screens for today matches

--- src/components/matches/MatchesToday.tsx
+++ src/components/matches/MatchesToday.tsx
@@ -1,76 +1,12 @@
-import React, { useEffect } from 'react';
-import MatchCard from './Match/MatchCard';
+import React from 'react';
 import { MobileLiveMatches } from './MobileLiveMatches/MobileLiveMatches';
-import { IMatch } from '../../models/model-interfaces';
-import { useViewport } from '../../utils/hooks/useViewport';
-import { selectMatchesBeingPlayed, useTtcDispatch, useTtcSelector } from '../../utils/hooks/storeHooks';
-import { setSetting } from '../../reducers/configReducer';
+import { selectMatchesBeingPlayed, useTtcSelector } from '../../utils/hooks/storeHooks';
 
 export const MatchesToday = () => {
-  const viewport = useViewport();
-  const dispatch = useTtcDispatch();
-  const isMobile = viewport.width < 768;
-
-  useEffect(() => {
-    if (!isMobile) {
-      dispatch(setSetting({key: 'container100PerWidth', value: true}));
-      return () => {
-        dispatch(setSetting({key: 'container100PerWidth', value: false}));
-      };
-    }
-    return undefined;
-  }, [isMobile]);
-
   const matchesToday = useTtcSelector(selectMatchesBeingPlayed);
   if (matchesToday.length === 0) {
     return <div />;
   }
 
-  if (isMobile) {
-    return <MobileLiveMatches matches={matchesToday} />;
-  }
-
-  if (viewport.width > 1500) {
-    return (
-      <div>
-        <BigMatches matches={matchesToday.slice(0, 2)} />
-        {matchesToday.length > 2 && <BigMatches matches={matchesToday.slice(2)} />}
-      </div>
-    );
-  }
-
-  // Medium devices
-  return (
-    <div className="row">
-      {matchesToday.map(match => (
-        <div className="col-lg-6" style={{paddingBottom: 5, paddingTop: 5}} key={match.id}>
-          <MatchCard
-            match={match}
-            isOpen={false}
-            viewportWidthContainerCount={viewport.width > 1200 ? 2 : 1}
-            viewport={viewport}
-          />
-        </div>
-      ))}
-    </div>
-  );
-};
-
-const BigMatches = ({matches}: {matches: IMatch[]}) => {
-  const viewport = useViewport();
-  return (
-    <div className="row">
-      {matches.map(match => (
-        <div className="col-md-6" style={{paddingBottom: 5, paddingTop: 5}} key={match.id}>
-          <MatchCard
-            match={match}
-            isOpen
-            viewportWidthContainerCount={2}
-            big
-            viewport={viewport}
-          />
-        </div>
-      ))}
-    </div>
-  );
+  return <MobileLiveMatches matches={matchesToday} />;
 };
--- src/components/matches/MobileLiveMatches/MobileLiveMatches.tsx
+++ src/components/matches/MobileLiveMatches/MobileLiveMatches.tsx
@@ -1,15 +1,28 @@
 import React from 'react';
 import { IMatch } from '../../../models/model-interfaces';
 import { MobileLiveMatchCard } from './MobileLiveMatchCard';
+import { useViewport } from '../../../utils/hooks/useViewport';
 
 type MobileLiveMatchesProps = {
   matches: IMatch[];
 };
 
-export const MobileLiveMatches = ({ matches }: MobileLiveMatchesProps) => (
-  <div style={{ display: 'flex', flexDirection: 'column', gap: 16, paddingTop: 10 }}>
-    {matches.map(match => (
-      <MobileLiveMatchCard key={match.id} match={match} />
-    ))}
-  </div>
-);
+export const MobileLiveMatches = ({ matches }: MobileLiveMatchesProps) => {
+  const viewport = useViewport();
+  const isMobile = viewport.width < 992;
+
+  return (
+    <div
+      style={{
+        display: 'grid',
+        gridTemplateColumns: isMobile ? '1fr' : 'repeat(2, 1fr)',
+        gap: 16,
+        paddingTop: 10,
+      }}
+    >
+      {matches.map(match => (
+        <MobileLiveMatchCard key={match.id} match={match} />
+      ))}
+    </div>
+  );
+};
