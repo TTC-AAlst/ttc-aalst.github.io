a96ed30715b366a239ad707da1759212ba2238c0
Laoujin <woutervs@hotmail.com>
2026-02-22 20:18:09 +0100

    replace match card with vandaag card

--- src/components/matches/Match/RoutedMatchCard.tsx
+++ src/components/matches/Match/RoutedMatchCard.tsx
@@ -1,27 +1,15 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
-import MatchCard from './MatchCard';
+import { MobileLiveMatchCard } from '../MobileLiveMatches/MobileLiveMatchCard';
 import { FullScreenSpinner } from '../../controls/controls/Spinner';
 import { NotFound } from '../../other/NotFound';
 import { selectMatches, useTtcSelector } from '../../../utils/hooks/storeHooks';
-import { useViewport } from '../../../utils/hooks/useViewport';
 
 export const RoutedMatchCard = () => {
   const params = useParams<{matchId: string, tabKey: string}>();
-  const viewport = useViewport();
   const matches = useTtcSelector(selectMatches);
   const initialLoad = useTtcSelector(state => state.config.initialLoad);
   const match = !!params.matchId && matches.find(x => x.id === parseInt(params.matchId as string, 10));
-
-  // TODO: fetch the match by id if it's not present in the store?
-  // const getMatch = (props) => {
-  //   const matchId = parseInt(props.match.params.matchId, 10);
-  //   const match = storeUtil.getMatch(matchId);
-  //   if (!match) {
-  //     this.props.fetchMatch(this.props.match.params.matchId);
-  //   }
-  //   return match;
-  // }
 
   if (!match) {
     if (initialLoad === 'done') {
@@ -32,11 +20,11 @@ export const RoutedMatchCard = () => {
 
   return (
     <div style={{marginBottom: 20, marginTop: 20, marginLeft: 5, marginRight: 5}}>
-      <MatchCard
+      <MobileLiveMatchCard
         match={match}
-        isOpen
-        params={params}
-        viewport={viewport}
+        expanded={true}
+        onToggle={() => {}}
+        isCollapsible={false}
       />
     </div>
   );
