d4d08d0c1b3aa68321729ad9715ac87845ffe6ef
Laoujin <woutervs@hotmail.com>
2026-06-18 00:54:15 +0200

    feat(db): migration for datetime(6) precision + nullable MatchPlayer.Status

--- backend/src/Ttc.DataAccess/Migrations/20260617225404_DateTimePrecisionAndNullableStatus.Designer.cs
+++ backend/src/Ttc.DataAccess/Migrations/20260617225404_DateTimePrecisionAndNullableStatus.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Ttc.DataAccess;
 
@@ -10,9 +11,11 @@ using Ttc.DataAccess;
 namespace Ttc.DataAccess.Migrations
 {
     [DbContext(typeof(TtcDbContext))]
-    partial class TtcDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260617225404_DateTimePrecisionAndNullableStatus")]
+    partial class DateTimePrecisionAndNullableStatus
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
